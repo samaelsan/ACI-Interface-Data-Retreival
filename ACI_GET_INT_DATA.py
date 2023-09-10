7f1ac52cf48f4bf2ccdc66004048115ed007cd44
samaelsan <113817488+samaelsan@users.noreply.github.com>
2023-09-10 01:08:58 -0600

    Update ACI_GET_INT_DATA.py

--- ACI_GET_INT_DATA.py
+++ ACI_GET_INT_DATA.py
@@ -1,14 +1,16 @@
 import aci_get_data_functions as aci
 if __name__ == "__main__":
+    base_url = "https://apic-ip-address/api"
+    username = "USERNAME"
+    password = "PASSWORD"
     interface_list = [ "1/1", "1/14", "1/15", "1/17"]
     node_list = ["1104", "1105"]
     pod_list = ["1"]
-    base_url = "https://apic-ip-address/api"
+
     headers = {
         'Content-Type': "application/json"
     }
-    username = "USERNAME"
-    password = "PASSWORD"
+
     cookie = aci.apic_login (base_url, username, password)
     pod_int_data = aci.get_pod_int_data(pod_list, node_list, interface_list, base_url, headers, cookie, username, password)
     df_pod_int_data = aci.get_data_csv(pod_int_data, "POD_INT_DATA.csv")
