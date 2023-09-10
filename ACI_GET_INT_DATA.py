b6b47d2f37c29c74c498893ec07501d52b12dbdc
samaelsan <113817488+samaelsan@users.noreply.github.com>
2023-09-10 01:10:06 -0600

    Update ACI_GET_INT_DATA.py

--- ACI_GET_INT_DATA.py
+++ ACI_GET_INT_DATA.py
@@ -1,8 +1,8 @@
 import aci_get_data_functions as aci
 if __name__ == "__main__":
-    base_url = "https://apic-ip-address/api"
-    username = "USERNAME"
-    password = "PASSWORD"
+    base_url = "https://your-apic-ip/api"
+    username = "your-username"
+    password = "your-password"
     interface_list = [ "1/1", "1/14", "1/15", "1/17"]
     node_list = ["1104", "1105"]
     pod_list = ["1"]
